187285adee71fdb12b015e7e696ee89af280afd8
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2019-01-13 18:08:18 +0100

    removed cheerio

--- lambda/custom/api/codingJam.js
+++ lambda/custom/api/codingJam.js
@@ -1,68 +1,25 @@
 const request = require('request')
-const cheerio = require('cheerio')
 
-const URL = 'http://codingjam.it/'
-
-const getPost = url => new Promise((resolve, reject) => {
-  request(url, function (error, response, html) {
-    if (error) {
-      reject(error)
-      return
-    }
-
-    const $ = cheerio.load(html)
-
-    const text =
-        $('.entry-content')
-          .first()
-          .text()
-          .replace(/\r?\n|\r/g, '')
-          .replace(/\t/g, '')
-
-    const title = $('.entry-title').first().text()
-
-    resolve({
-      text,
-      title
-    })
-  })
-})
+const REQUEST_OPTIONS = {
+  url: 'http://codingjam.it/wp-json/wp/v2/posts/',
+  json: true
+}
 
 const getTitles = () => new Promise((resolve, reject) => {
-  request(URL, function (error, response, html) {
+  request(REQUEST_OPTIONS, (error, response, body) => {
     if (error) {
       reject(error)
       return
     }
 
-    const $ = cheerio.load(html)
-
-    const titles = $('.entry-title').map((i, elem) => {
-      return cheerio.load(elem).text()
-    }).get()
+    const titles = body.map(item => {
+      return item.title.rendered
+    })
 
     resolve(titles)
   })
 })
 
-const getFirstPostUrl = () => new Promise((resolve, reject) => {
-  request(URL, function (error, response, html) {
-    if (error) {
-      reject(error)
-      return
-    }
-
-    const $ = cheerio.load(html)
-
-    const anchor = $('.entry-title a').first()
-
-    resolve(anchor.attr('href'))
-  })
-})
-
-const getFirtPost = () => getFirstPostUrl().then(getPost)
-
 module.exports = {
-  getFirtPost,
   getTitles
 }
